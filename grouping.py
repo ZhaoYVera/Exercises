f0278e4ba8da27ff20a66c7f73a73844b29b4645
ZhaoYVera <vera6878@163.com>
2020-07-31 14:34:37 +0800

    commit 202007311434.

--- grouping.py
+++ grouping.py
@@ -4,14 +4,16 @@ from collections import defaultdict
 
 
 class Grouping:
-    def __init__(self, ll, m: int, check_func=None):
-        self.n = len(ll)
+    def __init__(self, ll, m: float, check_func=None):
+        self.combinations = []
+        if any(x > m for x in ll):
+            return
         self.ll = sorted(ll)
+        self.n = len(ll)
         self.max = m
         self.num_parts = ceil(sum(ll)/m)
         self.check = check_func
         self.results = defaultdict(list)
-        self.combinations = []
         self.combine()
 
     # def check_feasible(self, lst):
@@ -23,7 +25,8 @@ class Grouping:
         s.t. sum of values in each part no more than m
         """
         if self.num_parts == 1:
-            self.combinations.append(self.ll)
+            if self.check is None or self.check():
+                self.combinations.append((self.ll, ))
             return
         self.results[1].append(([self.ll[-1]], self.ll[:-1]))
         self.make_new_pattern(0, [self.ll[-1]], [], self.ll[:-1], 1)
@@ -34,33 +37,32 @@ class Grouping:
                 self.make_new_pattern(0, [item[-1][-1]], item, item[-1][:-1], i)
             i += 1
         for item in self.results[self.num_parts-1]:
-            if self.check(item[-1], self.max):
-                self.combinations.append(item)
+            if sum(item[-1]) <= self.max:
+                if self.check is None or self.check():
+                    self.combinations.append(item)
 
-    def make_new_pattern(self, pos, new_pattern, assigned_pattern, rest, i):
+    def make_new_pattern(self, pos, old_pattern, assigned_pattern, rest, i):
         if pos == len(rest):
             return
-        new_pattern.append(rest[pos])
-        del rest[pos]
-        if self.check(new_pattern, self.max):
-            self.results[i].append((*assigned_pattern[:-1], new_pattern.copy(), rest.copy()))
-            self.make_new_pattern(pos, new_pattern, assigned_pattern, rest, i)
-        else:
-            rest.insert(pos, new_pattern.pop())
-            return
-        rest.insert(pos, new_pattern.pop())
-        self.make_new_pattern(pos + 1, new_pattern, assigned_pattern, rest, i)
+        new_pattern = old_pattern + [rest[pos]]
+        if sum(new_pattern) <= self.max:
+            if self.check is None or self.check():
+                self.results[i].append((*assigned_pattern[:-1], new_pattern, rest[:pos]+rest[pos+1:]))
+                self.make_new_pattern(pos, new_pattern, assigned_pattern, rest[:pos]+rest[pos+1:], i)
+                self.make_new_pattern(pos + 1, old_pattern, assigned_pattern, rest, i)
 
 
 if __name__ == '__main__':
-    n = 0
-    def check_feasible(lst, m):
-        global n
-        n += 1
-        return sum(lst) <= m
-    import random
-    cats = [random.random() for _ in range(9)]
-    r = Grouping(cats, 3, check_feasible)
-    for combination in r.combinations:
-        print(combination)
-    print(n)
+    # n = 0
+    # def check_feasible(lst, m):
+    #     global n
+    #     n += 1
+    #     return sum(lst) <= m
+    # import random
+    # cats = [random.random() for _ in range(9)]
+    # r = Grouping(cats, 3, check_feasible)
+    # for combination in r.combinations:
+    #     print(combination)
+    # print(n)
+    miaos = list(np.arange(3)+1.7)
+    rm = Grouping(miaos, 8)
